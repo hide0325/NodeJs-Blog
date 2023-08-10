b796c31b63d864b11550ac44632cceacac89ada8
hide0325 <tmnftc@yahoo.co.jp>
2023-08-10 14:40:52 +0900

    finished PART6

--- server/routes/main.js
+++ server/routes/main.js
@@ -39,10 +39,6 @@ router.get("", async (req, res) => {
   }
 });
 
-router.get("/about", (req, res) => {
-  res.render("about");
-});
-
 // router.get("", async (req, res) => {
 //   const locals = {
 //     title: "NodeJs Blog",
@@ -57,9 +53,60 @@ router.get("/about", (req, res) => {
 //   }
 // });
 
-// router.get("/about", (req, res) => {
-//   res.render("about");
-// });
+/*
+ * GET /
+ * Post :id
+ */
+router.get("/post/:id", async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description: "Simple Blog created with NodeJs, Express & MongoDb",
+    };
+
+    res.render("post", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/*
+ * GET /
+ * Post - searchTerm
+ */
+router.post("/search", async (req, res) => {
+  try {
+    const locals = {
+      title: "Search",
+      description: "Simple Blog created with NodeJs, Express & MongoDb",
+    };
+
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g, "");
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+        { body: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+      ],
+    });
+
+    res.render("search", {
+      data,
+      locals,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/about", (req, res) => {
+  res.render("about");
+});
 
 // function insertPostData() {
 //   Post.insertMany([
